df068af96bea66a26018abf1201ac6ba06379b06
asegui20923-coder <asegui20923@iesjoanramis.org>
2025-11-12 13:39:15 +0100

    1

--- prova1.py
+++ prova1.py
@@ -4,6 +4,9 @@ float(input("Escriure el tercer nombre. "))
 c= a + b
 print("El resultat de la suma {} + {} es {}".format(a, b, c))
 c= a * b
+if c>20:
+    print(La suma es major que 20, que es)
+else:
 print("El resultat de la multiplicacio {} * {} es {}".format(a, b, c))
 
                                                             
