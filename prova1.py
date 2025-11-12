01666459c3cdc045b2bed79e8222a8a61adb0b09
asegui20923-coder <asegui20923@iesjoanramis.org>
2025-11-12 13:20:06 +0100

    1

---
+++ prova1.py
@@ -0,0 +1,9 @@
+a= float(input("Escriure el primer nombre: "))
+b= float(input("Escriure el segon nombre. "))
+float(input("Escriure el tercer nombre. "))
+c= a + b
+print("El resultat de la suma {} + {} es {}".format(a, b, c))
+c= a * b
+print("El resultat de la multiplicacio {} * {} es {}".format(a, b, c))
+
+                                                            
