ab862b5733e76524564d52abf6e6842f3a62a13e
asegui20923-coder <asegui20923@iesjoanramis.org>
2025-11-12 13:43:27 +0100

    ex1.a

--- prova1.py
+++ prova1.py
@@ -3,10 +3,14 @@ b= float(input("Escriure el segon nombre. "))
 float(input("Escriure el tercer nombre. "))
 c= a + b
 print("El resultat de la suma {} + {} es {}".format(a, b, c))
-c= a * b
 if c>20:
-    print(La suma es major que 20, que es)
+    print("La suma es major que 20, que es")
 else:
+    print("La suma es major que 20, que es")
+c= a * b
 print("El resultat de la multiplicacio {} * {} es {}".format(a, b, c))
-
+if c>100:
+    print("La suma es major que 20, que es")
+else:
+    print("La suma es major que 20, que es")
                                                             
